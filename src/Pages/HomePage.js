6d17479916987e4ea0476b3fe334831531358740
blienmoges <blienmoges92@gmail.com>
2024-03-16 11:14:23 +0300

    updated

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
 import MovieCard from "../components/MovieCard";
 import AddFavourite from "../components/AddFavourite";
 import { Header } from "../components/Header";
@@ -7,26 +6,12 @@ import { Header } from "../components/Header";
 export const HomePage = () => {
   const API_URL =
     "https://api.themoviedb.org/3/movie/popular?api_key=b7234b3d0b8e2719c5518a5e91c26528&language=en-US";
-=======
-import { Header } from "../components/Header";
-import MovieCard from "../components/MovieCard";
-import { Search } from "../components/search/Search";
-import Avatar from "../Assets/Avatar.png";
-
-export const HomePage = () => {
-  const API_URL =
-    "https://api.themoviedb.org/3/movie/popular?api_key=b7234b3d0b8e2719c5518a5e91c26528&language=en-US&page=2";
->>>>>>> 7ea93fad21958976883269384ddd4ff69f9d0845
   const Top_Rated_Api =
     "https://api.themoviedb.org/3/movie/top_rated?api_key=b7234b3d0b8e2719c5518a5e91c26528&language=en-US";
 
   const [movies, setMovies] = useState([]);
   const [Top, setTop] = useState([]);
 
-<<<<<<< HEAD
-=======
-  const [searchQuery, setSearchQuery] = useState([]);
->>>>>>> 7ea93fad21958976883269384ddd4ff69f9d0845
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
@@ -45,7 +30,6 @@ export const HomePage = () => {
         setTop(data.results);
       })
       .catch((error) => {
-<<<<<<< HEAD
         console.error("Error fetching top rated movies:", error);
       });
   }, []);
@@ -94,64 +78,6 @@ export const HomePage = () => {
           />
         ))}
       </div>
-=======
-        console.error("Error fetching Top rated movies:", error);
-      });
-  }, []);
-
-  //  console.log(searchQuery)
-
-  return (
-    <div className=" relative">
-      <Header />
-      <div className="relatve">
-        <img
-          src={Avatar}
-          alt=""
-          className="-z-30 absolute w-full h-[500px] object-fill "
-        />
-      </div>
-      <div className=" top-0 left-0 w-full z-10">
-        <Search setSearchQuery={setSearchQuery} />
-      </div>
-      {searchQuery.length > 0 ? (
-        <div className="ml-5 mt-2 ">
-          <div className="text-4xl font-normal text-center text-white mt-[100px] mb-2 h-7 ">
-            <h1>Search result</h1>
-          </div>
-          <div className="flex gap-4 mb-[50px]  flex-wrap justify-start">
-            {searchQuery.map((movieReq) => (
-              <MovieCard movie={movieReq} />
-            ))}
-          </div>
-        </div>
-      ) : (
-        <>
-          <div className="ml-5 mt-28 ">
-            <div>
-              <h1 className="text-4xl font-normal text-center text-white mb-3  border-gray-400 border-2">
-                Top Rated movies
-              </h1>
-            </div>
-            <div className="flex gap-4 flex-wrap justify-center ">
-              {Top.map((movieReq) => (
-                <MovieCard movie={movieReq} />
-              ))}
-            </div>
-          </div>
-          <div className="ml-5 mt-2">
-            <div className="text-4xl font-normal w-full text-white mb-6 border-gray-400 border-2 h-[55px] mt-4 flex justify-center items-center">
-              <h1>popular movies</h1>
-            </div>
-            <div className="flex gap-4 mb-[50px]  flex-wrap justify-start">
-              {movies.map((movieReq) => (
-                <MovieCard movie={movieReq} />
-              ))}
-            </div>
-          </div>
-        </>
-      )}
->>>>>>> 7ea93fad21958976883269384ddd4ff69f9d0845
     </div>
   );
 };
